c4bb1adccb8e947ec56e89e43e6f863b1f4ea617
Ashley Knapp <ajknapp@ucsc.edu>
2024-10-16 16:19:45 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -22,33 +22,46 @@ const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
 
-let cursorX = 0;
-let cursorY = 0;
+// Stroke recording and Drawing
+const drawingChanged = new Event("drawing-changed");
+let strokes: number[][][] = [[]];
+let numStroke = 0;
 let isDrawing = false;
 
+// Record Strokes
 canvas.addEventListener("mousedown", (event) => {
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
+    strokes[numStroke].push([event.offsetX, event.offsetY]);
     isDrawing = true;
+    console.log("mouseDown");
 });
 
 canvas.addEventListener("mousemove", (event) => {
-if (isDrawing) {
-    drawLine(ctx, cursorX, cursorY, event.offsetX, event.offsetY);
-    cursorX = event.offsetX;
-    cursorY = event.offsetY;
+    if (isDrawing) {
+        strokes[numStroke].push([event.offsetX, event.offsetY]);
+        canvas.dispatchEvent(drawingChanged);
     }
 });
 
 canvas.addEventListener("mouseup", (event) => {
-if (isDrawing) {
-    drawLine(ctx, cursorX, cursorY, event.offsetX, event.offsetY);
-    cursorX = 0;
-    cursorY = 0;
-    isDrawing = false;
+    if (isDrawing) {
+        strokes[numStroke].push([event.offsetX, event.offsetY]);
+        isDrawing = false;
+        strokes.push([]);
+        numStroke++;
     }
 });
 
+// Clear and redraws all lines as instructed
+canvas.addEventListener("drawing-changed", function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < strokes.length; i++){
+        for (let j = 1; j < strokes[i].length; j++){
+            drawLine(ctx, strokes[i][j-1][0], strokes[i][j-1][1], strokes[i][j][0], strokes[i][j][1]);
+        }
+    }
+});
+
+// Draws a line from (x1, y1) to (x2, y2)
 function drawLine(line: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
     line.beginPath();
     line.moveTo(x1, y1);
@@ -65,5 +78,7 @@ clrBtn.innerHTML = "clear";
 app.append(clrBtn);
 
 clrBtn.addEventListener("click", function () {
+    strokes = [[]];
+    numStroke = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
