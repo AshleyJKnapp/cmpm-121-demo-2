1d35b2f2d1d44c166169b918db3dc27a7301502f
Ashley Knapp <ajknapp@ucsc.edu>
2024-10-16 01:54:07 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import "./style.css";
 
-const APP_NAME = "Hello! Testing project!";
+// General Set Up
+const APP_NAME = "ashley's silly little drawing app for class";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+const header = document.createElement("h1");
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+header.innerHTML = "Let's get cooking (drawing)"
+app.append(header);
+
+// Canvas Set Up
+const canvas = document.createElement("canvas");
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+
+canvas.height = canvas.width = 256;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 256, 256);
+app.append(canvas);
