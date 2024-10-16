96c0da6c3c6b453412e0bb8b4a8085f7c59e0a7e
Ashley Knapp <ajknapp@ucsc.edu>
2024-10-15 23:32:41 -0700

    Forgot a step, testing deployment now

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello! Testing project";
+const APP_NAME = "Hello! Testing project!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
